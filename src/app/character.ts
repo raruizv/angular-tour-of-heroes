4b2357bfb3ee1be7ddd1ae9295aa6650f4029ea0
raruizv <rafaelruiz@outlook.com>
2023-08-18 13:55:40 -0500

    Se ajustan modulos para los enuncuiados de la semana 20

--- src/app/character.ts
+++ src/app/character.ts
@@ -1,5 +1,8 @@
 export interface Character {
 	id: number;
 	name: string;
-	militarGrade: string;	
+	description: string,
+	militarGrade: string;
+	makings: string;
+	battle: string;	
 }
--- src/app/character/character.component.ts
+++ src/app/character/character.component.ts
@@ -8,11 +8,24 @@ import { Character } from '../character';
 })
 export class CharacterComponent implements OnInit {
 
-	character: Character = {
+	saladino: Character = {
 		id: 1,
 		name: 'Saladino',
-		militarGrade: 'Sultan de Egipto y Siria' 
+		description: 'Uno de los mas reconocidos lideres militares del medioevo. Monarca de la dinastia de los Ayubies y conquistador musulman en Oriente Medio',
+		militarGrade: 'Sultan de Egipto y Siria',
+		makings: 'Estrategia eficaz, diplomacia, generosidad, entre otras',
+		battle: 'Cuernos de Hattin'		
 	};
+	
+	rommel: Character = {
+		id: 2,
+		name: 'Erwin Rommel',
+		description: 'Apodado el \'zorro del desierto\' por la tropas aliadas. Hizo del frente africano un terreno de combate prolongado y desgastante. Acusado de conspirar contra el régimen nazi',
+		militarGrade: 'Mariscal de campo Ejercito Alemán',
+		makings: 'Liderazgo, formacion y criterio; decisiones sorpresa',
+		battle: 'El Alamein'		
+	};
+	
 	constructor(){ }
 	ngOnInit(){ }
 }
